0137293fe31f7073f043bef6d475262d2c978de2
Louis Dupraz <louis.dupraz@epitech.eu>
2024-02-15 01:19:48 +0100

    fix: added cleanup

--- get-rules-no-docker.sh
+++ get-rules-no-docker.sh
@@ -2,6 +2,8 @@
 
 if [ "$#" == "1" ]; then
   if [ "$1" == "--update-rules" ]; then
+    sudo rm -rf /tmp/banana-coding-style-checker
+    sudo rm -rf /tmp/lambdananas
     cd /tmp
     git clone https://github.com/Epitech/banana-coding-style-checker.git
     sudo mv banana-coding-style-checker/vera /usr/local/lib/vera++
